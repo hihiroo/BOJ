c1748851699bbb586dd5a48c38e47340a55b5ab0
hihiroo <alsrud606@naver.com>
2019-10-20 18:40:50 +0900

    내심과 외심은 사랑입니다 / 기하, 원주각과 내심의 정의

---
+++ MATH/16480.cpp
@@ -0,0 +1,13 @@
+// 외심과 내심은 사랑입니다.
+
+#include<bits/stdc++.h>
+using namespace std;
+#define lli long long int
+
+//D^2 = R(R-2r) 증명은 검색하면 나옴(원주각과 내심의 정의 이용)
+
+int main(){
+    lli R,r;
+    cin>>R>>r;
+    cout<<R*(R-2*r);
+}
