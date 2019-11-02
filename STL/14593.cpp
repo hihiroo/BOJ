0cc27c86d90ef2d42574a793e05b1f36443f5303
hihiroo <alsrud606@naver.com>
2019-11-02 11:52:21 +0900

    2017 아주대학교 프로그래밍 경시대회 large / vector, pair, struct, sort 기초 문제

---
+++ STL/14593.cpp
@@ -0,0 +1,27 @@
+// 2017아주대학교 프로그래밍 경시대회 large
+
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define mp make_pair
+#define lli long long int
+#define fst first
+#define snd second
+
+int main(){
+    int n,a,b,c;
+    vector<pair<int,pair<int,pair<int,int> > > > v(10001);
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        cin>>a>>b>>c;
+        v[i].first = -a;
+        v[i].snd.fst = b;
+        v[i].snd.snd.fst = c;
+        v[i].snd.snd.snd = i;
+    }
+    sort(v.begin()+1,v.begin()+n+1);
+    printf("%d",v[1].snd.snd.snd);
+}
+
+
+
