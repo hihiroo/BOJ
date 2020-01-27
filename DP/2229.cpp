2ff4e4fc37bf379b7fe94bceecc6cfda14b4f1a9
hihiroo <alsrud606@naver.com>
2020-01-27 23:41:31 +0900

    조 짜기 / 1차 dp 응용 이전 코드에서 TLE가 뜨지 않은 이유는 시간복잡도가 10^6이었기 때문 2차 dp로 구현했지만 e값이 바뀌지 않기 때문에 1차 dp나 마찬가지였음

--- DP/2229.cpp
+++ DP/2229.cpp
@@ -8,27 +8,25 @@ using namespace std;
 #define fst first
 #define snd second
 
-int dp[1005][1005], arr[1005];
+int dp[1005], arr[1005],N;
 
-int f(int s, int e){
-    if(s >= e) return 0;
-    if(dp[s][e] != -1) return dp[s][e];
+int f(int s){
+    if(s > N) return 0;
+    if(dp[s] != -1) return dp[s];
     
     int min_ = 10000, max_ = 0;
-    for(int i=s; i<=e; i++){
+    for(int i=s; i<=N; i++){
         min_ = min(min_,arr[i]);
         max_ = max(max_,arr[i]);
-        dp[s][e] = max(dp[s][e],max_-min_+f(i+1,e));
+        dp[s] = max(dp[s],max_-min_+f(i+1));
     }
-    return dp[s][e];
+    return dp[s];
 }
 
 int main(){
-    int N;
     cin>>N;
     memset(dp,-1,sizeof(dp));
 
-    for(int i=1; i<=N; i++)
-        cin>>arr[i];
-    cout<<f(1,N);
+    for(int i=1; i<=N; i++) cin>>arr[i];
+    cout<<f(1);
 }
