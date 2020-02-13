449ec0d0e679c2cf5d248b8c0a38f5237941c58d
hihiroo <alsrud606@naver.com>
2020-02-14 03:45:01 +0900

    XOR / 레이지프로퍼게이션

--- Lazy_propagation/12844.cpp
+++ Lazy_propagation/12844.cpp
@@ -1,79 +1,75 @@
 //XOR
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 #define pb push_back
+#define pf push_front
 #define mp make_pair
 #define lli long long int
 #define fst first
 #define snd second
+#define pop_f pop_front
+#define pop_b pop_back
 
-int c[2002][2002],f[2002][2002],re = -1,z;
-vector<vector<int> > v(2002);
+int arr[2000001],size,lazy[2000001];
 
-int flow(int s, int e){
-    int ans=0;
-    while(1){
-        vector<int> par(2002,-1);
-        queue<int> q;
-        q.push(s);
-        par[s] = s;
-        while(!q.empty()){
-            int vtx = q.front();
-            q.pop();
-            if(par[e] != -1) break;
-            for(int i=0; i<v[vtx].size(); i++){
-                int next = v[vtx][i];
-                if(par[next] == -1 && c[vtx][next] > f[vtx][next]){
-                    q.push(next);
-                    par[next] = vtx;
-                }
-            }
-        }
-        if(par[e] == -1) break;
-        int poss = INT_MAX;
-        for(int i=e; i != s; i = par[i])
-            poss = min(poss,c[par[i]][i]-f[par[i]][i]);
-        for(int i=e; i!=s; i=par[i]){
-            f[par[i]][i] += poss;
-            f[i][par[i]] -= poss;
-        }
-        ans += poss;
-        if(re != -1){
-            re++;
-            if(re == z) return ans;
-        }
-    }
-    return ans;
+void construct(){
+    for(int i=size; i>0; i--)
+        arr[i] = arr[i*2]^arr[i*2+1];
 }
+void propagate(int vtx, int nL, int nR){
+    if(lazy[vtx]){
+        if(vtx <= size){
+            lazy[vtx*2] ^= lazy[vtx];
+            lazy[vtx*2+1] ^= lazy[vtx];
+        }
+        else arr[vtx] ^= lazy[vtx];
+        lazy[vtx] = 0;
+    }
+}
+int f(int L, int R, int vtx, int nL, int nR){
+    propagate(vtx,nL,nR);
+    if(L > nR || nL > R) return 0;
+    if(L <= nL && nR <= R) return arr[vtx];
+    int mid = (nL + nR)/2;
+    return f(L,R,vtx*2,nL,mid)^f(L,R,vtx*2+1,mid+1,nR);
+}
+void update(int L, int R, int val, int vtx,int nL, int nR){
+    propagate(vtx,nL,nR);
+    if(L > nR || nL > R) return;
+    if(L <= nL && nR <= R){
+        lazy[vtx] ^= val;
+        propagate(vtx,nL,nR);
+    }
+    else{
+        int mid = (nL + nR)/2;
+        update(L,R,val,vtx*2,nL,mid);
+        update(L,R,val,vtx*2+1,mid+1,nR);
+        arr[vtx] = arr[vtx*2]^arr[vtx*2+1];
+    }
+}
+
 
 int main(){
-    int n,m,a,k;
-    cin>>n>>m>>z;
-    for(int i=1; i<=n; i++){//사람과 소스0 연결
-        v[0].pb(i), v[i].pb(0);
-        c[0][i] = 1;
+    int n,m,t,a,b,c;
+    cin>>n;
+    int lv=0;
+    while(pow(2,lv) < n){
+        size += pow(2,lv);
+        lv++;
     }
-    for(int i=1; i<=m; i++){
-        v[n+i].pb(n+m+1); //일과 싱크 연결
-        v[n+m+1].pb(n+i);
-        c[n+i][n+m+1] = 1;
-    }
-
-    for(int i=1; i<=n; i++){//사람과 일 연결
-        cin>>k;
-        for(int j=1; j<=k; j++){
-            cin>>a;
-            v[i].pb(n+a),v[n+a].pb(i);
-            c[i][n+a] = 1;
-        }
-    }
-    int cnt = flow(0,n+m+1);
-    re = 0;
     for(int i=1; i<=n; i++)
-        c[0][i]+=z;
-    printf("%d",cnt + flow(0,n+m+1));
+        scanf("%d",&arr[size+i]);
+    construct();
+
+    cin>>m;
+    for(int i=0; i<m; i++){
+        scanf("%d%d%d",&t,&a,&b);
+        if(a > b) swap(a,b);
+        if(t == 1){
+            scanf("%d",&c);
+            update(a+1,b+1,c,1,1,size+1);
+        }
+        else printf("%d\n",f(a+1,b+1,1,1,size+1));
+    }
 }
-
-
 
