a3eee2758e1226f54e3bdf1ea70ccfc693fd7892
hihiroo <alsrud606@naver.com>
2020-04-14 21:59:09 +0900

    막대기 / 비트 1의 개수세기

---
+++ Basic/1094.cpp
@@ -0,0 +1,14 @@
+//막대기
+#include <bits/stdc++.h>
+#define mp make_pair
+#define pb push_back
+#define fst first
+#define snd second
+#define lli long long
+using namespace std;
+
+int main(){
+    int X;
+    cin >> X;
+    cout <<  __builtin_popcount(X);
+}
