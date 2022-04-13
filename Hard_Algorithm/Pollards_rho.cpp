01ad098f00236e78f81813c261fef86352fcbe28
hihiroo <alsrud606@naver.com>
2022-04-13 14:17:03 +0900

    큰 수 소인수분해 / Pollard's rho, 맞았습니다.
    오버플로우 주의(2^62까지 들어오기 때문에 곱셈 과정에서 오버플로우 발생 -> 시간초과. add 함수를 따로 만들어서 매 연산마다 모듈러로 나눠줌)
    폴라드 로 알고리즘으로 인수분해 진행함
    
    폴라드 로 알고리즘의 원리는 n = pq (p는 1이 아닌 n의 인수)라고 가정.
    f(x) = x^2 + c mod n(의사난수)라고 했을 때, f(x0), ff(x0), fff(x0), ... 로 이루어진 수열 X를 만들 수 있음
    이 때 X의 범위는 [0, n)이고 birthday paradox에 의해 O(sqrt(n)) 내에 사이클이 만들어짐
    X에 사이클이 존재하기 때문에 x0 mod p, x1 mod p, ... 수열도 O(sqrt(p)) 안에 사이클이 만들어짐.
    
    사이클을 찾는 방식은 토끼와 거북이 알고리즘(플로이드 알고리즘)을 이용
    토끼는 두 칸씩 이동하고 거북이는 한 칸씩 이동
    같은 수가 수열에 존재하면 반드시 사이클이 생기기 때문에 수열의 원소가 모두 다르다면 토끼와 거북이가 만날 일이 없음
    그러나 사이클이 있다면 O(N)안에 반드시 만남
    
    이 원리를 이용하여 X mod p 수열에서 사이클을 탐색
    x mod p = y mod p 일 때까지 탐색
    p가 어떤 수인지는 모르지만, x mod p = y mod p라면 abs(x-y)는 p의 배수이기 때문에 gcd(abs(x-y), n) != 1
    이 때 gcd 값이 p가 되고, 만약 p가 n이라면 인수를 찾지 못한 것이므로 의사난수 생성 함수의 파라미터인 x0, c 값을 바꿔서 재탐색
    
    p의 평균 값은 sqrt(n)이므로 사이클을 찾는 평균 시간복잡도는 O(n^(1/4))

--- Hard_Algorithm/Pollards_rho.cpp
+++ Hard_Algorithm/Pollards_rho.cpp
@@ -4,7 +4,23 @@ using namespace std;
 #define pb push_back
 
 namespace Miller_Rabin{
-    vector<lli> A = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
+    lli add(lli a, lli b, lli m){
+        a = a % m;
+        b = b % m;
+        if(a >= m-b) return a-(m-b); // if(a+b >= m) return a+b-m;
+        return a+b;
+    }
+
+    lli mul(lli a, lli b, lli m){
+        lli res = 0;
+        while(b > 0){
+            if(b & 1) res = add(res, a, m);
+            b = b >> 1;
+            a = add(a, a, m); // 2a -> 4a -> 8a ...
+        }
+        return res;
+    }
+
     /*
         return a^b mod m
         res = a^(2^0*b0 + 2^1*b1 + 2^2*b2 ...)
@@ -12,12 +28,13 @@ namespace Miller_Rabin{
     lli pow(lli a, lli b, lli m){
        lli res = 1;
        while(b > 0){
-           if(b & 1) res = (a*res) % m;
-           a = (a*a) % m;
+           if(b & 1) res = mul(res, a, m);
+           a = mul(a, a, m);
            b = b >> 1;
        }
        return res;
     }
+
     /*
         ai로 n이 합성수인지 테스트
         합성수이면 1, 아니면 0 return
@@ -30,12 +47,14 @@ namespace Miller_Rabin{
         if(tmp == 1 || tmp == n-1) return 0;
 
         for(int i=1; i<k; i++){
-            tmp *= tmp;
-            tmp %= n;
+            tmp = mul(tmp, tmp, n);
             if(tmp == n-1) return 0;
         }
         return 1;
     }
+
+    vector<lli> A = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
+    
     /*  Deterministic Miller-Rabin (n < 2^64)
         return 1 if n is prime, 0 otherwise
     */
@@ -61,9 +80,10 @@ namespace Miller_Rabin{
 }
 
 struct Pollards_rho{
+    #define mul Miller_Rabin::mul
     vector<lli> prime_factor;
 
-    void init(lli n){
+    Pollards_rho(lli n): prime_factor(0){
         factorize(n);
         sort(prime_factor.begin(), prime_factor.end());
     }
@@ -84,9 +104,9 @@ struct Pollards_rho{
         c = rand() % 10 + 1;
 
         while(g == 1){
-            x = ((x * x) % n + c) % n; // f(x) = x^2 + c mod n;
-            y = ((y * y) % n + c) % n;
-            y = ((y * y) % n + c) % n; // y는 두 번 이동
+            x = (mul(x, x, n) + c) % n; // f(x) = x^2 + c mod n;
+            y = (mul(y, y, n) + c) % n;
+            y = (mul(y, y, n) + c) % n; // y는 두 번 이동
             g = gcd(abs(y-x), n);
             // g값이 1이 아니면 y-x가 0이거나 n의 인수 중 하나인 p의 배수.
             // 즉, y % p == x % p로 사이클 생김
@@ -116,8 +136,7 @@ int main(){
     lli n;
     cin >> n;
 
-    Pollards_rho a;
-    a.init(n);
+    Pollards_rho a(n);
     for(int i=0; i<a.prime_factor.size(); i++){
         cout << a.prime_factor[i] << '\n';
     }
